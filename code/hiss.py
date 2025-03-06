63d9c0a09dec676cc9ce35eb84b63e642c5270d9
James Mugford <james@yozies.com>
2025-03-06 15:59:47 +0000

    Fixed hiss.py

--- code/hiss.py
+++ code/hiss.py
@@ -7,8 +7,6 @@ import time
 from talon import ctrl, actions
 from talon import tap, noise
 from talon.types import Point2d
-from talon_plugins import eye_mouse, eye_zoom_mouse
-from talon_plugins.eye_mouse import config, toggle_camera_overlay, toggle_control
 
 class NoiseModel:
     def __init__(self):
@@ -19,20 +17,7 @@ class NoiseModel:
         self.mouse_last = Point2d(0, 0)
         self.dragging = False
 
-        tap.register(tap.MMOVE, self.on_move)
         noise.register('hiss', self.on_noise)
-
-    def on_move(self, typ, e):
-        print('on_move')
-        # if typ != tap.MMOVE: return
-        # self.mouse_last = pos = Point2d(e.x, e.y)
-        # if self.hiss_start and not self.dragging:
-        #     if (pos - self.mouse_origin).len() > 10:
-        #         self.dragging = True
-        #         self.button = 0
-        #         x, y = self.mouse_origin.x, self.mouse_origin.y
-        #         ctrl.mouse(x, y)
-        #         #ctrl.mouse_click(x, y, button=0, down=True)
 
     def on_noise(self, noise):
         now = time.time()
@@ -44,14 +29,6 @@ class NoiseModel:
             #toggle_control1(True)
             #actions.tracking.control_enabled()
             # thisctrl.key_press("keypad_5")
-
-            # if now - self.hiss_last < 0.25:
-            #     ctrl.mouse_click(button=self.button, down=True)
-            #     self.hiss_last = now
-            #     self.dragging = True
-            # else:
-            #     self.mouse_origin = self.mouse_last
-            # self.hiss_start = now
         else:
             #eye_mouse.control_mouse.disable()
             actions.tracking.control1_toggle(False)
@@ -59,19 +36,6 @@ class NoiseModel:
             # ctrl.key_press("keypad_5")
             # ctrl.mouse_click(button=0)
             # print('hiss end')
-            # if self.dragging:
-            #     ctrl.mouse_click(button=self.button, up=True)
-            #     self.dragging = False
-            # else:
-            #     duration = time.time() - self.hiss_start
-            #     if duration > 0.5:
-            #         self.button = 1
-            #         #ctrl.mouse_click(button=1)55
-            #         self.hiss_last = now
-            #     elif duration > 0.2:
-            #         self.button = 0
-            #         ctrl.mouse_click(button=0)
-            #         self.hiss_last = now
-            # self.hiss_start = 0
+            
 
 model = NoiseModel()
--- command_mode.talon
+++ command_mode.talon
@@ -8,19 +8,11 @@ enter: key(enter)
 
 lock windows: key(super-l)
 
-
-page: key(pgdown)
-
 invert: key(super-alt-n)
 
 # sleep: key(plus)
 
 mouse: key(keypad_5)
-
-bash: key(alt)
-
-min: key(super-down)
-max: key(super-up)
 
 switch:
 	user.mouse_toggle_control_mouse()
@@ -37,21 +29,30 @@ parrot(tut):
 
 dredge: key(alt-tab)
 
-dictation start: key(alt-p)
-dictation stop: key(alt-o)
-
-pause: key(alt-1)
-recenter: key(alt-2)
+#	dictation start: key(alt-p)
+#	dictation stop: key(alt-o)
 
 track: key(f9)
 
 bulk message: key(f13)
 
+#	KinesicMouse
+pause | ram: key(alt-1)
+center: key(alt-2)
 
-# Function key hotkey mappings
+#basic: key(shift-alt-1)
+#mouse: key(shift-alt-2)
+#scroll: key(shift-alt-3)
+#test: key(shift-alt-4)
+
+
+
+# Function key hotkeyb mappings
 
 # f14 = some auto hotkey debug test
 
 key(f15): bcore.repeat_phrase(1)
 
+#	Stream Deck
 key(f16): speech.toggle()
+
