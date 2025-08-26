3be38b95c679f692de294d955426f32ffe63cb9d
James Mugford <james@yozies.com>
2025-08-26 13:04:51 +0100

    cleanup

--- eye_tracking_setting.py
+++
@@ -1,8 +0,0 @@
-from talon import app
-from talon.track.geom import Point2d
-from talon_plugins import speech, eye_mouse, eye_zoom_mouse
-
-eye_zoom_mouse.config.screen_area = Point2d(200, 150 )
-eye_zoom_mouse.config.img_scale = 4.5
-
-eye_zoom_mouse.toggle_zoom_mouse(1);
